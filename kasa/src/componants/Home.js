2b62fc2233df381ac34d3f39efcd3e739f7fc867
Emerick Junger <emerick.junger@gmail.com>
2022-10-31 00:34:56 +0100

    main en cours

--- kasa/src/componants/Home.js
+++ kasa/src/componants/Home.js
@@ -1,7 +1,16 @@
+//import { Link } from "react-router-dom";
+import Cote from "../assets/IMG.jpg";
+import Background from "../assets/Background.jpg";
+
 function Home() {
 
     return (
         <>
+          <figure>
+            <img src={Cote} alt=""/>
+            <h1>Chez vous, partout et ailleurs</h1>
+            <div className="filter"></div>
+          </figure>
         </>
     );
   };
