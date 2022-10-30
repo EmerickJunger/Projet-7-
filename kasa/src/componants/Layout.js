b51b0a15e18fc8efc3e078bab80b5f8b1038d616
Emerick Junger <emerick.junger@gmail.com>
2022-10-30 17:30:51 +0100

    css

--- kasa/src/componants/Layout.js
+++ kasa/src/componants/Layout.js
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom";
+import Logo from "../assets/LOGO.png";
 
 function Layout() {
 
     return (
         <>
         <header>
-            <div></div>
+            <nav>
+                <a class="logo" href="/">
+                    <img src={Logo} alt=""/>
+                </a>
+                <ul class="menu">
+                    <li class="survol"><a href="#hebergements">Accueil</a></li>
+                    <li class="survol"><a href="#activites">À propos</a></li>
+                </ul>
+            </nav>   
         </header>
         <main>
             <Outlet/>
