aa57513cb94f2f0b68de91e652808d19b3d62086
Emerick Junger <emerick.junger@gmail.com>
2022-11-04 15:15:17 +0100

    place en cours

--- kasa/src/componants/Place.js
+++ kasa/src/componants/Place.js
@@ -6,13 +6,19 @@ function Place() {
           <figure className="logis">
 
           </figure>
-          <h2>
+          <h2 className="place">
             Lieu, avec précision
           </h2>
-          <h3>
+          <h3 className="town">
             Ville, région
           </h3>
-          
+          <div className="filtrage">
+            <ul className="filtres">
+              <li className="filtre">Détail</li>
+              <li className="filtre">Détail</li>
+              <li className="filtre">Détail</li>
+            </ul>
+          </div>
         </div>
         </>
     );
